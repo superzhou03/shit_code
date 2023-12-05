c7b641bc488996d300412304efbb0c43f16c20cb
superzhou03 <weirong.zhou@outlook.com>
2023-12-05 23:26:59 +0800

    leecode704

--- array/704.cpp
+++ array/704.cpp
@@ -1,5 +1,18 @@
 // Author: superzhou03
-// Created time: 2023:12:05
-// 
-#include<iostream>
-
+// Created time: 2023-12-05
+class Solution {
+public:
+    int search(vector<int>& nums,int target){
+        int left = 0, right = nums.size() - 1, middle = 0;
+        while(left <= right){
+            middle = (left + right) / 2;
+            if(nums[middle] > target)
+                right = middle - 1;
+            else if(nums[middle] < target)
+                left = middle + 1;
+            else 
+                return middle;
+        }
+        return -1;
+    }
+}
