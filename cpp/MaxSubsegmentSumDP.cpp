26c78be686b9197aac4bdd4d6afab9b1c89d89ee
superzhou03 <150320955+superzhou03@users.noreply.github.com>
2023-12-06 14:52:47 +0800

    Update MaxSubsegmentSumDP.cpp

--- cpp/MaxSubsegmentSumDP.cpp
+++ cpp/MaxSubsegmentSumDP.cpp
@@ -1,25 +1,25 @@
 // Author: superzhou03
 // Created time: 2023-12-06
 /*
-  最大子段和-DP方法
-  Description
-  给出N个数字, 计算出最大的子段和。
+ * 最大子段和-DP方法
+ * Description
+ * 给出N个数字, 计算出最大的子段和。
  
-  Input
-  第一行给出一个数字 T(1<=T<=20) 代表接下来的组数.
-  接下来每 T 行,开始给出一个正整数 N(1<=N<=100000), 接着跟着N个整数.。
-  数据保证中间结果和最后结果都在int范围内。
-  Output
-  输出最大的子段和
- 
-  Sample Input
-  2
-  5 6 -1 5 4 -7
-  7 0 6 -1 1 -6 7 -5
-  Sample Output
-  14
-  7
-*/
+ * Input
+ *  第一行给出一个数字 T(1<=T<=20) 代表接下来的组数.
+ *  接下来每 T 行,开始给出一个正整数 N(1<=N<=100000), 接着跟着N个整数.。
+ *  数据保证中间结果和最后结果都在int范围内。
+ *  Output
+ * 输出最大的子段和
+ *
+ * Sample Input
+ * 2
+ * 5 6 -1 5 4 -7
+ * 7 0 6 -1 1 -6 7 -5
+ * Sample Output
+ * 14
+ * 7
+ */
 #include <iostream>
 #include <vector>
 #include <climits>
