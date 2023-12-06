943348a71c0191a4f99f874836620e3073406202
superzhou03 <weirong.zhou@outlook.com>
2023-12-06 21:22:11 +0800

    leecode No.27

--- array/27.cpp
+++ array/27.cpp
@@ -3,6 +3,10 @@
 class Solution{
     public:
         int removeElement(vector<int>& nums, int val){
-            
+           int fast = 0 ,slow = 0, lenth = nums.size();
+           for (int fast = 0; fast < lenth; fast++)
+                if(nums[fast] != val)
+                    nums[slow++] = nums[fast];
+                return slow;
         }
 }
