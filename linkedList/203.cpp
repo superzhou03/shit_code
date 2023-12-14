c96e6486248ad85cd5d735d96b7e5816a3767aaf
superzhou03 <weirong.zhou@outlook.com>
2023-12-14 20:28:56 +0800

    leecode No.203

--- linkedList/203.cpp
+++ linkedList/203.cpp
@@ -1,5 +1,7 @@
 // Author: superzhou03
 // Created time: 2023-12-14
+// 使用原链表删除头节点和中间节点操作不一样
+// 所以修改链表时最好新建一个虚拟头节点vHead进行操作
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -13,6 +15,17 @@
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-
+        ListNode *vHead = new ListNode();
+        vHead->next = head;
+        ListNode *current = vHead;
+        while (current->next != NULL) {
+            if (current->next->val == val)
+                current->next = current->next->next;
+            else
+                current = current->next;
+        }
+        // 不明白为什么释放current的空间之后代码会运行不起来
+        // delete current;
+        return vHead->next;
     }
 };
